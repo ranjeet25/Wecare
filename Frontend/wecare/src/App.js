e104c938b6600f54105c09a7c637a33ca58ddc1f
Ranjeet Saw <sawranjeet550@gmail.com>
2022-10-08 19:35:56 +0530

    Created react app

---
+++ Frontend/wecare/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Components/Test';
+
+function App() {
+  return (
+    <Test></Test>
+  );
+}
+
+export default App;
---
+++ Frontend/wecare/src/Components/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div> we are WECARE</div>
+  )
+}
+
+export default Test
