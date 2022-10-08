c335f93e664e7e9d8b8d8a422dcb261732e0b16b
Manav0702 <manavsanghvi4295@gmail.com>
2022-10-08 20:31:44 +0530

    added navbar

--- Frontend/wecare/src/App.js
+++ Frontend/wecare/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Landing from './Components/Landing'
 import Test from './Components/Test';
 
 function App() {
   return (
-    <Test></Test>
+    
+    <Landing></Landing>
+    
+    
   );
 }
 
