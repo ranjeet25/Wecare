c5e8dbef0da1e6ca79d683a0190872cfd8d6046e
Manav0702 <manavsanghvi4295@gmail.com>
2022-10-09 12:08:42 +0530

    added backend

--- Frontend/wecare/src/Components/Dashbox.js
+++ Frontend/wecare/src/Components/Dashbox.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import p1 from "../Assets/images/health-check.jpg";
+import p2 from "../Assets/images/online-doc.png";
+import p3 from "../Assets/images/foos.jpg";
+import p4 from "../Assets/images/disease.jpg";
+
 
 function Dashbox() {
   return (
@@ -8,10 +13,10 @@ function Dashbox() {
     <div className='row1 flex w-screen '>
     <div class="flex items-center scale-75">
   <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
-    <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+    <img class="rounded-xl" src={p1} alt="" />
     <div class="flex justify-between items-center">
       <div>
-        <h1 class="mt-5 text-2xl font-semibold">Aloe Cactus</h1>
+        <h1 class="mt-5 text-2xl font-semibold">Visit Now</h1>
         <p class="mt-2">$11.99</p>
       </div>
       <div>
@@ -22,7 +27,7 @@ function Dashbox() {
 </div>
 <div class=" flex items-center scale-75">
   <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
-    <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+    <img class="rounded-xl" src={p2} alt="" />
     <div class="flex justify-between items-center">
       <div>
         <h1 class="mt-5 text-2xl font-semibold">Aloe Cactus</h1>
@@ -40,7 +45,7 @@ function Dashbox() {
 <div className='row1 flex w-screen '>
     <div class="flex items-center scale-75">
   <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
-    <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+    <img class="rounded-xl" src={p3} alt="" />
     <div class="flex justify-between items-center">
       <div>
         <h1 class="mt-5 text-2xl font-semibold">Aloe Cactus</h1>
@@ -54,7 +59,7 @@ function Dashbox() {
 </div>
 <div class=" flex items-center scale-75">
   <div class="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
-    <img class="rounded-xl" src="https://images.unsplash.com/photo-1547517023-7ca0c162f816" alt="" />
+    <img class="rounded-xl" src={p4} alt="" />
     <div class="flex justify-between items-center">
       <div>
         <h1 class="mt-5 text-2xl font-semibold">Aloe Cactus</h1>
